298d396410399b53249fc33601cccbfb2cbde352
MADHAV <madhavgoel2006@gmail.com>
2025-10-25 12:48:16 +0200

    volume final

---
+++ 3_Volume of  a cylinder.py
@@ -0,0 +1,9 @@
+r = float(input("Enter the radius of the cylinder: "))
+h = float(input("Enter the height of the cylinder: "))
+
+# Calculate the volume
+pi = 3.14159  # approximate value of π
+volume = pi * (r ** 2) * h
+
+# Display the result
+print("The volume of the cylinder is:", volume)
