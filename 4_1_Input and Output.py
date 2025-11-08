1197bcd0054f82cfee7ac2bb69dd370e14291d16
MADHAV <madhavgoel2006@gmail.com>
2025-11-08 09:22:00 +0100

    Revert "input and output final commit"
    This reverts commit 58a206d9268e379c710d7fc6387ee67e23cb0adb.

--- 4_1_Input and Output.py
+++
@@ -1,9 +0,0 @@
-# Ask for user inputs
-name = input("Enter your name: ")
-greeting = input("Enter your preferred greeting: ")
-
-# Combine using an f-string
-message = f"{greeting},{name}!"
-
-# Display the custom message
-print(message)
